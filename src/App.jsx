9e495f4ce737fe4fd26a8b41dcb7612fbe175a3d
Iva-Kamenova <iva.kamenova2004@abv.bg>
2025-08-05 17:13:55 +0300

    Accessibility for keyboard users.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { nanoid } from "nanoid";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 const FILTER_MAP = {
   All: () => true,
@@ -73,6 +82,14 @@ function App(props) {
 
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length < prevTaskLength) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
 
   return (
     <div className="todoapp stack-large">
@@ -81,7 +98,10 @@ function App(props) {
 
       <div className="filters btn-group stack-exception">{filterList}</div>
 
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+        {headingText}
+      </h2>
+
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
